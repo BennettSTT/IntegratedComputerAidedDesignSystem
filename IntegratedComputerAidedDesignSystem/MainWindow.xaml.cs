80b2cb3ab05d236eb95b1c0b634445eea00c7f43
Sazonov Artem <bennettst@yandex.ru>
2020-10-17 19:54:19 +0700

    Updated UI

--- IntegratedComputerAidedDesignSystem/MainWindow.xaml.cs
+++ IntegratedComputerAidedDesignSystem/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using IntegratedComputerAidedDesignSystem.Infrastructure;
-using IntegratedComputerAidedDesignSystem.Infrastructure.Parsers;
 using Microsoft.Win32;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace IntegratedComputerAidedDesignSystem
 {
@@ -11,12 +13,14 @@ namespace IntegratedComputerAidedDesignSystem
     /// </summary>
     public partial class MainWindow
     {
+        private string _text;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
@@ -25,12 +29,89 @@ namespace IntegratedComputerAidedDesignSystem
 
             if (openFileDialog.ShowDialog() != true) return;
 
-            var text = await ReadFileAsync(openFileDialog.FileName);
+            _text = await ReadFileAsync(openFileDialog.FileName);
 
-            var manager = new MatrixManager(text);
+        }
 
-            var qMatrixInfo = manager.GetMatrixInfo(MatrixType.Q);
-            var rMatrixInfo = manager.GetMatrixInfo(MatrixType.R);
+        private static void RenderGrid(Grid grid, MatrixInfo matrixInfo)
+        {
+            grid.ColumnDefinitions.Clear();
+            grid.RowDefinitions.Clear();
+            grid.Children.Clear();
+
+            grid.ShowGridLines = true;
+
+            for (var i = 0; i < matrixInfo.Columns.Length + 1; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Width = new GridLength(60),
+                });
+            }
+
+            for (var i = 0; i < matrixInfo.Rows.Length + 1; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition
+                {
+                    Height = new GridLength(35)
+                });
+            }
+
+            for (var i = 0; i < matrixInfo.Columns.Length; i++)
+            {
+                var column = matrixInfo.Columns[i];
+                TextBlock txtBlock = new TextBlock
+                {
+                    Text = column,
+                    FontSize = 14,
+                    Foreground = new SolidColorBrush(Colors.Black),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+
+                Grid.SetRow(txtBlock, 0);
+                Grid.SetColumn(txtBlock, i + 1);
+
+                grid.Children.Add(txtBlock);
+            }
+
+            for (var i = 0; i < matrixInfo.Rows.Length; i++)
+            {
+                var row = matrixInfo.Rows[i];
+                TextBlock txtBlock = new TextBlock
+                {
+                    Text = row,
+                    FontSize = 14,
+                    Foreground = new SolidColorBrush(Colors.Black),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+
+                Grid.SetRow(txtBlock, i + 1);
+                Grid.SetColumn(txtBlock, 0);
+
+                grid.Children.Add(txtBlock);
+            }
+
+            for (var i = 0; i < matrixInfo.Matrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrixInfo.Matrix.GetLength(1); j++)
+                {
+                    TextBlock txtBlock = new TextBlock
+                    {
+                        Text = matrixInfo.Matrix[i, j].ToString(),
+                        FontSize = 14,
+                        Foreground = new SolidColorBrush(Colors.Black),
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                    };
+
+                    Grid.SetRow(txtBlock, i + 1);
+                    Grid.SetColumn(txtBlock, j + 1);
+
+                    grid.Children.Add(txtBlock);
+                }
+            }
         }
 
         private static async Task<string> ReadFileAsync(string filePath)
@@ -39,6 +120,18 @@ namespace IntegratedComputerAidedDesignSystem
             {
                 return await reader.ReadToEndAsync();
             }
+        }
+
+        private void RenderQMatrix(object sender, RoutedEventArgs e) => RenderMatrix(MatrixType.Q);
+        private void RenderRMatrix(object sender, RoutedEventArgs e) => RenderMatrix(MatrixType.R);
+
+        private void RenderMatrix(MatrixType matrixType)
+        {
+            var manager = new MatrixManager(_text);
+            var matrixInfo = manager.GetMatrixInfo(matrixType);
+
+            RenderGrid(Grid, matrixInfo);
+            MatrixNameLabel.Content = matrixType.ToString();
         }
     }
 }
